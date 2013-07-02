b1200bbaf14d6b8a19f09e29edc9c231944b75da
Harmony Golmard <emilie.golmard@gmail.com>
2013-07-02 19:39:16 +0200

    Messages d'erreurs formulaires + nouvelle classe erreur

--- tests/PHP/insert/news2.php
+++ tests/PHP/insert/news2.php
@@ -2,35 +2,115 @@
 $titre = "Administration";
 include ("../includes/header.php");
 include ("../includes/menu.php");
-
-echo '<h1>Ajout d\'un évènement</h1>';
-
-// connexion à la BDD MySql
 include ("../includes/sql.php");
 
 // récupération des variables postées dans le formulaire
 // TODO : contrôles d'erreurs
-$nom = $connexion->real_escape_string($_POST['nom']);
-$prenom = $connexion->real_escape_string($_POST['prenom']);
-$datedeb = $connexion->real_escape_string($_POST['datedeb']);
-$datefin = $connexion->real_escape_string($_POST['datefin']);
-$divers = $connexion->real_escape_string($_POST['divers']);
+$nom=strip_tags($_POST['nom']);
+$prenom=strip_tags($_POST['prenom']);
+$datedeb = strip_tags($_POST['datedeb']);
+$datefin = strip_tags($_POST['datefin']);
+$divers = strip_tags($_POST['divers']);
+
 
 if ($datefin == "null"){
 $datefin = "";
 }
 
-// requete ajout d'actualité
+// requete ajout d'actualité + envoi
 $requete = "INSERT INTO evenement (nom,prenom,datedeb,datefin,divers) VALUES ('$nom', '$prenom','$datedeb','$datefin','$divers')";
-
-//echo $requete;
-
-// envoi de la requete
 $connexion->query($requete) or die ('Erreur '.$requete.' '.$connexion->error);
 
 // ok
-echo '<p>Merci, l\'evenement '.$nom.' a bien été ajouté !</p>';
+echo '<p>Merci, l\'evenement '.$nom. .$prenom. ' a bien été ajouté !</p>';
+
+
+// Gestion des erreurs
+
+//Nom//
+
+	if ($nom == '') {
+		echo '<p class="erreur">Nom non renseigné</p>';
+	}
+
+	else{
+		echo 'nom : ' .$nom.' bien rajouté ';
+	}
+
+	echo '<br/>';
+
+
+
+//Prénom//
+
+	if ($prenom == '') {
+		echo '<p class="erreur"> PRénom non renseigné</p>';
+	}
+
+	else{
+		echo 'prénom : ' .$prénom.' bien rajouté ';
+	}
+
+	echo '<br/>';
+
+
+
+//datedeb//
+
+
+	if(is_numeric($datedeb))
+	{
+		echo 'Date début : '.$datedeb.' ajoutée ';
+	}
+
+	elseif (!is_numeric($datedeb))
+	{
+		echo '<p class="erreur"> L\'année renseignée n\'est pas valide</p>'; 
+	}
+
+	if ($datedeb == '') 
+		{
+			echo '<p class="erreur">Naissance non renseignée</p>';
+		}
+		
+
+		echo '<hr/>';
+
+
+
+//datefin//
+
+
+	if(is_numeric($datefin))
+	{
+		echo 'Date de fin : '.$datefin.' ajoutée ';
+	}
+
+	elseif (!is_numeric($datefin))
+	{
+		echo '<p class="erreur"> L\'année renseignée n\'est pas valide</p>'; 
+	}
+
+	if ($datefin == '') 
+		{
+			echo '<p class="erreur">Naissance non renseignée</p>';
+		}
+		
+
+		echo '<hr/>';
+
+
+// Divers
+
+
+
+	if ($divers == '') 
+	{
+		echo '<p class="erreur">Vous n avez laissé aucun commentaire</p>';
+	}
+	
+	echo '<hr/>';
 
 
 include ("../includes/footer.php");
-?>
+?>	
